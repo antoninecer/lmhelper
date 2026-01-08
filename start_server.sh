5edc3e5c2f4ccefcbbec693a563c676104e7415a
Antonín Ečer <antoninecer@gmail.com>
2026-01-08 18:05:16 +0100

    pridany shel na start api a webu

---
+++ start_server.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd /Users/tonda/work/lmhelper
+source venv/bin/activate
+cd /Users/tonda/work/lmhelper/embedding
+python3 rag_server.py
+
---
+++ start_web.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+cd /Users/tonda/work/lmhelper/code
+./start_php.sh
+
